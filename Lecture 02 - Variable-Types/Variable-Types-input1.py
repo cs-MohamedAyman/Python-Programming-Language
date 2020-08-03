69726f340e51f417a1f5f8a2a174ab48dd576817
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2020-08-03 20:55:59 +0200

    version 2020

---
+++ Lecture 02 - Variable-Types/Variable-Types-input1.py
@@ -0,0 +1,4 @@
+name = str(input())
+age = int(input())
+weight = float(input())
+print(name, age, weight)
---
+++ Lecture 02 - Variable-Types/Variable-Types-input3.py
@@ -0,0 +1,4 @@
+name, age, weight = input().split()
+age = int(age)
+weight = float(weight)
+print(name, age, weight, sep = '\n')
