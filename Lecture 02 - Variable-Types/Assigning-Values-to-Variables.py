d81999804bdd1e675004e2cb1d581b77c8a58070
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2020-08-26 13:43:56 +0200

    Create Assigning-Values-to-Variables.py

---
+++ Lecture 02 - Variable-Types/Assigning-Values-to-Variables.py
@@ -0,0 +1,29 @@
+#Assigning Values to Variables
+
+url = 'www.google.com'
+print(url)
+
+n = 5
+print(n)
+
+#Multiple Assignment
+
+a, b, c = 5, 3.2, "Hello"
+
+print(a)
+print(b)
+print(c)
+
+x = y = z = "same"
+
+print(x)
+print(y)
+print(z)
+
+#Python Constant
+
+PI = 3.14
+GRAVITY = 9.8
+
+print(PI)
+print(GRAVITY)
