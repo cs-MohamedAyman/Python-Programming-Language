498310fe7c68b3dfbc65ddf87aa04995dd924659
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2020-08-27 09:50:36 +0200

    Create Logical-Operators.py

---
+++ Lecture 03 - Basic-Operations/Logical-Operators.py
@@ -0,0 +1,13 @@
+#Logical Operators
+
+x, y = True, False
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+
+a, b, c, d = 10, 5, 7, 3
+print(a > b and c < d)
+print(a > b or c < d)
+print(not a > b)
+print(not c < d)
