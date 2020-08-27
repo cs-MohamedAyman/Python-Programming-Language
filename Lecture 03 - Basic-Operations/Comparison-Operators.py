db7199f9edeaecb35d0c948daa2bd2eae5524eae
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2020-08-27 09:24:56 +0200

    Create Comparison-Operators.py

---
+++ Lecture 03 - Basic-Operations/Comparison-Operators.py
@@ -0,0 +1,17 @@
+#Comparison Operators
+
+x, y = 21, 10
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x, y = 'a', 'd'
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
