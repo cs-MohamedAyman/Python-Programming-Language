6879f0b5754de0ed8a46263327a60cc6cae6c66b
Mohamed Ayman <sw.eng.MohamedAyman@gmail.com>
2021-03-18 14:47:38 +0200

    Update Multi-Sudoko-Game-Answer.py

--- Projects/Multi-Sudoko-Game/Multi-Sudoko-Game-Answer.py
+++ Projects/Multi-Sudoko-Game/Multi-Sudoko-Game-Answer.py
@@ -1,14 +1,25 @@
 import random
 root_N = 3
 N = root_N * root_N
-mode = 33
+mode, n_modes = 0, 35
 grids, cpy_grids = [], []
 similar_boxes, n_grids, graphs = [], [], []
 symbols = ['.', '1', '2', '3', '4', '5', '6', '7', \
            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-#This function sets the configuration of Sudoku Game grids 
+#This function checks if the given mode is valid or not 
+def check_valid_mode(x):
+    return 0 <= x < n_modes
+
+#This function sets the configuration of Multi-Sudoku Game grids 
 def set_config():
+    global mode
+    mode = int(input('Enter the mode index: '))
+    while not check_valid_mode(mode):
+        mode = int(input('Enter a valid mode index'))
+
+#This function contracts the modes of Multi-Sudoku Game grids 
+def contract_modes():
     NL = '\n'
     #mode 00
     graphs.append('* * *'+NL+
@@ -387,19 +398,15 @@ def check_valid_position(i, j):
     return 0 <= i < N and 0 <= j < N
 
 #This function checks if the given grid index is valid or not 
-def check_valid_grid_index(grid_idx=0):
+def check_valid_grid_index(grid_idx):
     return 0 <= grid_idx < n_grids[mode]
 
-#This function checks if the given cell is empty or not 
-def check_empty_cell(i, j, grid_idx=0):
-    return grids[grid_idx][i][j] == 0
-
 #This function checks if the given cell is original or not
-def check_original_cell(i, j, grid_idx=0):
+def check_original_cell(grid_idx, i, j):
     return cpy_grids[grid_idx][i][j] != 0
 
 #This function checks if the given value is valid with the given cell
-def check_valid_value(i, j, v, grid_idx=0):
+def check_valid_value(grid_idx, i, j, v):
 	#Check delete case
     if v == 0:
         return True
@@ -439,12 +446,12 @@ def check_valid_value_cell(grid_idx_s, box_idx_s, grid_idx_t, box_idx_t, i, j, v
     if i_s <= i < i_s+root_N and j_s <= j < j_s+root_N:
         step_i = i_t - i_s
         step_j = j_t - j_s
-        return check_valid_value(i+step_i, j+step_j, v, grid_idx_t)
+        return check_valid_value(grid_idx_t, i+step_i, j+step_j, v)
     return True
 
 #This function checks if the given value is valid with the shared boxes in the other grids
-def check_valid_value_in_shared_boxes(i, j, v, grid_idx=0):
-    res = check_valid_value(i, j, v, grid_idx)
+def check_valid_value_in_shared_boxes(grid_idx, i, j, v):
+    res = check_valid_value(grid_idx, i, j, v)
     for grid_i, box_i, grid_j, box_j in similar_boxes[mode]:
         if grid_idx == grid_i:
             res &= check_valid_value_cell(grid_i, box_i, grid_j, box_j, i, j, v)
@@ -464,7 +471,7 @@ def cpy_cell(grid_idx_s, box_idx_s, grid_idx_t, box_idx_t, i, j, set_original=Fa
             cpy_grids[grid_idx_t][i+step_i][j+step_j] = cpy_grids[grid_idx_s][i][j]
 
 #This function copies the shared boxes in the other grids
-def cpy_cell_in_shared_boxes(i, j, v, grid_idx=0):
+def cpy_cell_in_shared_boxes(grid_idx, i, j, v):
     for grid_i, box_i, grid_j, box_j in similar_boxes[mode]:
         if grid_idx == grid_i:
             cpy_cell(grid_i, box_i, grid_j, box_j, i, j)
@@ -472,31 +479,31 @@ def cpy_cell_in_shared_boxes(i, j, v, grid_idx=0):
             cpy_cell(grid_j, box_j, grid_i, box_i, i, j)
 
 #This function sets the given value to the given cell
-def set_cell(i, j, v, grid_idx=0):
+def set_cell(grid_idx, i, j, v):
     grids[grid_idx][i][j] = v
-    cpy_cell_in_shared_boxes(i, j, v, grid_idx)
+    cpy_cell_in_shared_boxes(grid_idx, i, j, v)
 
 #This function solves the grid
-def solve_grid(i, j, grid_idx=0):
+def solve_grid(grid_idx, i, j):
     if j == N:
         i += 1
         j = 0
     if i == N:
         return True
-    if check_original_cell(i, j, grid_idx):
-        return solve_grid(i, j+1, grid_idx)
+    if check_original_cell(grid_idx, i, j):
+        return solve_grid(grid_idx, i, j+1)
     for k in range(1, N+1):
-        if check_valid_value_in_shared_boxes(i, j, k, grid_idx):
+        if check_valid_value_in_shared_boxes(grid_idx, i, j, k):
             grids[grid_idx][i][j] = k
             cpy_grids[grid_idx][i][j] = k
-            if solve_grid(i, j+1, grid_idx):
+            if solve_grid(grid_idx, i, j+1):
                 return True
             grids[grid_idx][i][j] = 0
             cpy_grids[grid_idx][i][j] = 0
     return False
 
 #This function gets the similar boxes the other grids
-def get_similar_boxes(box_idx, grid_idx=0):
+def get_similar_boxes(grid_idx, box_idx):
     res = []
     for grid_i, box_i, grid_j, box_j in similar_boxes[mode]:
         if grid_idx == grid_i and box_idx == box_i:
@@ -506,7 +513,7 @@ def get_similar_boxes(box_idx, grid_idx=0):
     return res    
 
 #This function copies the given box in the shared boxes in the other grids
-def cpy_box_in_shared_boxes(box_idx, shared_boxes, grid_idx=0, set_original=False):
+def cpy_box_in_shared_boxes(grid_idx, box_idx, shared_boxes, set_original=False):
     for grid_k, box_k in shared_boxes:
         a, b = cvt_box_to_grid_pos(box_idx)
         for i in range(a, a+root_N):
@@ -514,7 +521,7 @@ def cpy_box_in_shared_boxes(box_idx, shared_boxes, grid_idx=0, set_original=Fals
                 cpy_cell(grid_idx, box_idx, grid_k, box_k, i, j, True)
 
 #This function checks if the given box is empty or not
-def is_empty_box(box_idx, grid_idx=0):
+def is_empty_box(grid_idx, box_idx):
     a, b = cvt_box_to_grid_pos(box_idx)
     for i in range(a, a+root_N):
         for j in range(b, b+root_N):
@@ -523,7 +530,7 @@ def is_empty_box(box_idx, grid_idx=0):
     return True
 
 #This function checks if the given box is divided into empty cells and non-empty cells or not
-def is_mix_box(box_idx, grid_idx=0):
+def is_mix_box(grid_idx, box_idx):
     empty_cells = 0
     a, b = cvt_box_to_grid_pos(box_idx)
     for i in range(a, a+root_N):
@@ -537,20 +544,20 @@ def cpy_similar_boxes(grid_idx):
     for i in range(0, N, root_N):
         for j in range(0, N, root_N):
             box_idx = cvt_grid_pos_to_box(i, j)
-            similar_boxes = get_similar_boxes(box_idx, grid_idx)
-            cpy_box_in_shared_boxes(box_idx, similar_boxes, grid_idx, True)
+            similar_boxes = get_similar_boxes(grid_idx, box_idx)
+            cpy_box_in_shared_boxes(grid_idx, box_idx, similar_boxes, True)
 
 #This function generates cells in the grids
 def generate_cells(grid_idx=0):
     for k in range(0, N, root_N):
         box_idx = cvt_grid_pos_to_box(k, k)
-        if not is_empty_box(box_idx, grid_idx):
+        if not is_empty_box(grid_idx, box_idx):
             continue
         for i in range(root_N):
             for j in range(root_N):
                 n = random.randint(1, N)
-                while not check_valid_value_in_shared_boxes(k+i, k+j, n, grid_idx) or \
-                      check_original_cell(k+i, k+j, grid_idx):
+                while not check_valid_value_in_shared_boxes(grid_idx, k+i, k+j, n) or \
+                      check_original_cell(grid_idx, k+i, k+j):
                     n = random.randint(1, N)
                 grids[grid_idx][k+i][k+j] = n
                 cpy_grids[grid_idx][k+i][k+j] = n
@@ -561,11 +568,11 @@ def remove_cells(grid_idx=0):
         for i in range(0, N, root_N):
             for j in range(0, N, root_N):
                 box_idx = cvt_grid_pos_to_box(i, j)
-                if is_mix_box(box_idx, grid_idx):
+                if is_mix_box(grid_idx, box_idx):
                     continue
                 t1 = random.randint(0, root_N-1)
                 t2 = random.randint(0, root_N-1)
-                while not check_original_cell(i+t1, j+t2, grid_idx):
+                while not check_original_cell(grid_idx, i+t1, j+t2):
                     t1 = random.randint(0, root_N-1)
                     t2 = random.randint(0, root_N-1)
                 grids[grid_idx][i+t1][j+t2] = 0
@@ -574,10 +581,10 @@ def remove_cells(grid_idx=0):
 #This function generates cells in the given grid
 def generate_grids():
     #Generate cells in the grid
-    generate_cells(0)    
+    generate_cells()    
     for grid_idx in range(n_grids[mode]):
         #Solve the complete grid
-        solve_grid(0, 0, grid_idx)
+        solve_grid(grid_idx, 0, 0)
         #Copy the similar boxes in the another grids
         cpy_similar_boxes(grid_idx)
     for grid_idx in range(n_grids[mode]):
@@ -596,7 +603,7 @@ def grid_clear():
 def read_input():
     grid_idx, i, j, v = map(int, input('Enter the row index and column index and value: ').split())
     while not check_valid_grid_index(grid_idx) or not check_valid_position(i, j) or \
-          not check_valid_value_in_shared_boxes(i, j, v, grid_idx) or check_original_cell(i, j, grid_idx):
+          not check_valid_value_in_shared_boxes(grid_idx, i, j, v) or check_original_cell(grid_idx, i, j):
         grid_idx, i, j, v = map(int, input('Enter a valid row index and column index and value: ').split())
     return grid_idx, i, j, v
 
@@ -612,7 +619,7 @@ def play_game():
         #Read an input from the player
         grid_idx, i, j, v = read_input()
         #Set the input position with the value
-        set_cell(i, j, v, grid_idx)
+        set_cell(grid_idx, i, j, v)
         #Check if the grid has a win state
         if check_win():
             #Prints the grids
@@ -623,6 +630,7 @@ def play_game():
 
 while True:
     set_config()
+    contract_modes()
     grid_clear()
     generate_grids()
     play_game()
